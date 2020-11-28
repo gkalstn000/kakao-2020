15b77d4938c55e835a5ae2d5c7cea433ff9e8992
gkalstn000 <gkalstn000@gmail.com>
2020-11-28 14:18:56 +0900

    7 done

--- 2020 kakao ques7.py
+++ 2020 kakao ques7.py
@@ -1,50 +1,32 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 26 17:59:02 2020
-
-@author: hms
-"""
-def move(state) :
-    h, w, direction, cost = state
-        
-    
-    
-    
-    
-    
-    
-    if Board[h][w] == 1 or (w == 0 and h == 0) : return False
-    if direction == 'u' :
-        return 0 <= w < len(Board) and 0 <= h < len(Board)-1 and Board[h-1][w] != 1
-    elif direction == 'd' :
-        return 0 <= w < len(Board) and 1 <= h < len(Board) and Board[h+1][w] != 1 
-    elif direction == 'r' :
-        return 0 <= w < len(Board)-1 and 0 <= h < len(Board) and Board[h][w+1] != 1
-    else :
-        return 1 <= w < len(Board) and 0 <= h < len(Board) and Board[h][w-1] != 1 
-
-    
-    
-    
+import numpy as np
+from collections import deque
+def sort_(state) : return sorted(state, key=lambda x : (x[0], x[1]))
+def add(x, y) : return (x[0]+y[0], x[1]+y[1])
+def check(head, tail) : return Board[head[0], head[1]] == 0 and Board[tail[0], tail[1]] == 0
+def check_(state, move, cord) : return cord != state and cord not in move
+def moving(head, tail) :
+    state = sort_([head, tail])
+    moving = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    move = [sort_([add(head,x), add(tail, x)]) for x in moving if check(add(head, x), add(tail, x))]
+    rotating = [sort_([head, add(head, x)]) for x in moving  if check(head, add(head, x)) and check(tail, add(tail, x)) and check_(state, move, sort_([head, add(head, x)]))] +\
+               [sort_([tail, add(tail, x)]) for x in moving  if check(head, add(head, x)) and check(tail, add(tail, x)) and check_(state, move, sort_([head, add(head, x)]))]
+    return move+rotating
 def solution(board):
-    global Board
-    Board = board
-    
-    init = [[0, 0, 'r', 0]]
-    
-    
-    
-    return 0
-
+    global Board, N
+    N = len(board)
+    Board = np.pad(board, ((1,1),(1,1)), 'constant', constant_values=1)
+    que = deque([[(1, 1), (1, 2), 0]])
+    visted = [[(1, 1), (1, 2)]]
+    while que:
+        head, tail, cost  = que.popleft()
+        if head == (N, N) or tail == (N, N):
+            return cost
+        for child in moving(head, tail):
+            if child not in visted:
+                que.append([*child, cost+1])
+                visted.append(child)
+                
+#board = [[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]
 board = [[0, 0, 0, 1, 1],[0, 0, 0, 1, 0],[0, 1, 0, 1, 1],[1, 1, 0, 0, 1],[0, 0, 0, 0, 0]]	
-solution(board)
 
-'''
-    DIRECTION = {'up':'down', 'down':'up', 'right':'left', 'left':'right'}
-    ORDER = {'HC' : 'HCC', 'HCC' : 'HC', 'TC' : 'TCC', 'TCC' : 'TC'}
-'''
-ord('r')
-ord('l')
-ord('u')
-ord('d')
+print(solution(board))
